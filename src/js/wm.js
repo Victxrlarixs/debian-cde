c718ad3c08eafcf62ee5674c03c990f3d277ee94
Victor Larios <35201083+Victxrlarixs@users.noreply.github.com>
2026-02-11 01:25:49 -0600

    separate css
    js features fixed, separate css

--- src/js/wm.js
+++ src/js/wm.js
@@ -44,3 +44,16 @@ function stopDrag() {
     document.removeEventListener("mousemove", move);
     document.removeEventListener("mouseup", stopDrag);
 }
+
+
+/* ================= CLOCK ================= */
+
+function updateClock() {
+    const now = new Date();
+    const h = now.getHours().toString().padStart(2, "0");
+    const m = now.getMinutes().toString().padStart(2, "0");
+    document.getElementById("clock").textContent = `${h}:${m}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
